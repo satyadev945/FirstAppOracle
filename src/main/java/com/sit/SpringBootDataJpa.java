6fa65c020a48309c624052d7db029abf385fe6fb
Modernize Studio <studio@modernize.com>
2026-09-07 14:51:34 +0000

    Commit for planId: 165

--- src/main/java/com/sit/SpringBootDataJpa.java
+++ src/main/java/com/sit/SpringBootDataJpa.java
@@ -2,36 +2,17 @@ package com.sit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.context.ApplicationContext;
-//import org.springframework.context.ConfigurableApplicationContext;
-//import com.sit.Service.IDoctorService;
-//import com.sit.entity.Doctor;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.sit.entity")
+@EnableJpaRepositories(basePackages = "com.sit.Repository")
 public class SpringBootDataJpa {
 
 	public static void main(String[] args) {
-		
-		SpringApplication.run(SpringBootDataJpa.class, args);
-		
-		//ApplicationContext ctx=SpringApplication.run(SpringBootDataJpa.class, args);
 
-		//		IDoctorService ser=ctx.getBean("doctorService",IDoctorService.class);
-		//		try {
-		//			Doctor doc=new Doctor();
-		//			doc.setDocName("sairam");
-		//			doc.setSpecialization("MD_Cardio");
-		//			doc.setIncome(90000.00);
-		//
-		//			String Result=ser.registerDoctor(doc);
-		//			System.out.println(Result);
-		//
-		//		}
-		//		catch(Exception e) {e.printStackTrace();}
-		//
-		//		((ConfigurableApplicationContext) ctx).close();
-		//
-		//
+		SpringApplication.run(SpringBootDataJpa.class, args);
+
 	}
 }
-

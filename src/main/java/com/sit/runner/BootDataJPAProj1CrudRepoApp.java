bb0c2dc4edc28d9cb406d5d3ae914cb25bf78c76
Modernize Studio <studio@modernize.com>
2026-09-18 12:09:47 +0000

    Commit for planId: 123

--- src/main/java/com/sit/runner/BootDataJPAProj1CrudRepoApp.java
+++ src/main/java/com/sit/runner/BootDataJPAProj1CrudRepoApp.java
@@ -3,8 +3,6 @@ package com.sit.runner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.AutoConfigureOrder;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Component;
